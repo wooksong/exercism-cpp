42bc7604578d33dcca2cf5637f07fc77f112b56d
Wook Song <wook16.song@samsung.com>
2022-10-02 12:24:23 +0900

    Easy: Add a solution for the exercise, Two Fer

---
+++ two-fer/two_fer.h
@@ -0,0 +1,10 @@
+#if !defined(TWO_FER_H)
+#define TWO_FER_H
+
+#include <iostream>
+
+namespace two_fer {
+std::string two_fer(const std::string &name = "you");
+}  // namespace two_fer
+
+#endif  // TWO_FER_H
